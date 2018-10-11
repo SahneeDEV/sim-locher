56d1521abc96f15dc441cd013edc430fa38759ac
Patrick Sachs <kontakt@patrick-sachs.de>
2018-10-11 20:40:03 +0200

    Locher verwendet PapierStapel

--- SimLocher.java
+++ SimLocher.java
@@ -1,6 +1,38 @@
+/**
+ * Der eigentliche Locher. Er ist in der Lage Papier Stapel zu lochen.
+ */
 public class SimLocher
 {
-    public void steckPapierRein(Papier papier) {
-        //papier.lochen();
+    private PapierStapel stapel;
+
+    /**
+     * Legt einen Papier Stapel in den Locher ein.
+     * @param stapel Der Papier Stapel.
+     */
+    public void einlegen(PapierStapel stapel) {
+        this.stapel = stapel;
+    }
+
+    /**
+     * Entnimmt den aktuellen Papier Stapel aus dem Locher.
+     * @return Der entnommene Papier Stapel.
+     */
+    public PapierStapel entnehmen() {
+        PapierStapel stapel = this.stapel;
+        this.stapel = null;
+        return stapel;
+    }
+
+    /**
+     * Gibt den aktuell sich im Locher befindlichen Papier Stapel zurück.
+     * @return Der aktuelle Papier Stapel.
+     */
+    public PapierStapel getStapel() {
+        return this.stapel;
+    }
+
+    /** locht... */
+    public void lochen() {
+        // Stapel lochen ....
     }
 }
