23b82b447b857af17b02b6021ea1b20f2ee38684
Patrick Sachs <kontakt@patrick-sachs.de>
2018-10-11 20:09:13 +0200

    PapierStapel ist generisch, dass nur ein Typ Papier abgelegt werden kann

--- PapierStapel.java
+++ PapierStapel.java
@@ -1,21 +1,22 @@
 import java.util.ArrayList;
 
 /**
- * Mehrere Papiere auf einmal um sie lochen zu können.
+ * Mehrere Papiere eines Types auf einmal um sie lochen zu können.
  */
-public class PapierStapel
+public class PapierStapel<TPapier extends Papier>
 {
-    private final ArrayList<Papier> papiere;
+    private final ArrayList<TPapier> papiere;
     
     public PapierStapel() {
-        this.papiere = new ArrayList<Papier>();
+        this.papiere = new ArrayList<TPapier>();
     }
     
     /**
      * Legt ein Blatt Papier auf dem Stapel ab. Wenn das Papier breits auf dem Stapel ist, wird es nicht abgelegt.
+     * @param papier Das Papier, welches auf dem Stapel abgelegt werden soll.
      * @return true wenn das Papier abgelegt wurde, sonst false.
      */
-    public boolean ablegen(Papier papier) {
+    public boolean ablegen(TPapier papier) {
         if (papiere.contains(papier)) {
             return false;
         }
@@ -25,14 +26,16 @@ public class PapierStapel
     
     /**
      * Entnimmt das Papier aus dem Stapel wenn es darauf liegt.
+     * @param papier Das Papier, welches vom Stapel entnommen werden soll.
      * @return true wenn das Papier entnommen wurde, sonst false.
      */
-    public boolean entnehmen(Papier papier) {
+    public boolean entnehmen(TPapier papier) {
         return papiere.remove(papier);
     }
     
     /**
      * Gibt die Größe des Papierstapels zurück.
+     * @return Die Anzahl
      */
     public int groesse() {
         return papiere.size();
