108c8230b71b68dc9a53a867ba75265667f24394
DaNeubi <Lukas.Neubauer97@hotmail.de>
2018-10-13 17:01:50 +0200

    basic structure

---
+++ de/wolc/gui/Game.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.control.Button;
+import javafx.geometry.Insets;
+import javafx.geometry.*;
+import javafx.event.*;
+import javafx.scene.control.Labeled;
+
+public class Game{
+
+    private final String windowTitle = "World of Locher Craft";
+
+    public Scene GameMainStage(Stage stage){
+
+        return null;
+    }
+}
