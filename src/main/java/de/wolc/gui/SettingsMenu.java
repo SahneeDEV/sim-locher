55a1b113ba00a707f1d4d45f0eea045007fdf987
JakobHeubl <jakobheubl8@gmail.com>
2018-10-14 19:04:28 +0200

    Button Importiert
    Signed-off-by: JakobHeubl <jakobheubl8@gmail.com>

--- src/main/java/de/wolc/gui/SettingsMenu.java
+++ src/main/java/de/wolc/gui/SettingsMenu.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.CheckBox;
 import javafx.geometry.*;
+import javafx.scene.control.Button;
 import javafx.event.*;
 import javafx.scene.control.ProgressIndicator;
 
