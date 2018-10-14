6b004d474c176ceb0ca7d84d1e21b1bf5a48ee45
Patrick Sachs <kontakt@patrick-sachs.de>
2018-10-14 17:17:08 +0200

    Debugger integriert

--- scripts/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-mvn compile
-echo Running World of Lochercraft ...
-cd ../target/classes
-java de/wolc/gui/Gui
-cd ../../scripts
--- scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mvn compile
-echo Running World of Lochercraft ...
-find ../src -name "*.class" -delete
-find ../src -name "*.ctx" -delete
-cd ../target/classes
-java de/wolc/gui/Gui
-cd ../../scripts
