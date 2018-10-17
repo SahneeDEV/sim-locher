9967de4b987dd7e3c3a9efec5c8a7ff65343e851
DaNeubi <Lukas.Neubauer97@hotmail.de>
2018-10-17 22:44:47 +0200

    animation timer

--- src/main/java/de/wolc/gui/Game.java
+++ src/main/java/de/wolc/gui/Game.java
@@ -1,5 +1,6 @@
 package de.wolc.gui;
 
+import java.io.Console;
 import java.util.ArrayList;
 
 import de.wolc.MultiUse;
@@ -12,13 +13,26 @@ import javafx.scene.layout.*;
 import javafx.geometry.*;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
+import javafx.animation.AnimationTimer;
 
 public class Game{
 
     private final String windowTitle = "World of Locher Craft";
     private final String backgroundImageLocation = "de/wolc/gui/images/Test_Bild.jpg";
     private Spieler spieler;
-    private final double remainingTime = 30.00;
+    
+    //Game Variables
+    private int remainingTimeAvailable = 30;
+    private boolean hasLoched = false;
+
+    //Variables for Refreshing the Score
+    private int scoreValue;
+    private ArrayList<Konfetti> konfettiList;
+
+    //Variables for Countdown timer
+    private long lastNanoTimeTimer = 0;
+
+
 
     public Game () {
        this.spieler = new Spieler();
@@ -50,8 +64,8 @@ public class Game{
         VBox rightVBox = new VBox();
 
         //Creating Label for outputing score
-        ArrayList<Konfetti> konfettiList = spieler.getKonfetti();
-        int scoreValue = konfettiList.size();
+        konfettiList = spieler.getKonfetti();
+        scoreValue = konfettiList.size();
         Label score = new Label();
         score.setTextFill(Color.RED);
         score.setText("Score: "+scoreValue); 
@@ -59,7 +73,7 @@ public class Game{
         //Creating Label for remaining Time
         Label remainingTime = new Label();
         remainingTime.setTextFill(Color.RED);
-        remainingTime.setText("Zeit: 00:00");
+        remainingTime.setText("Zeit: 00:"+remainingTimeAvailable);
 
         //Adding remainingTime and score to VBox 
         rightVBox.getChildren().addAll(score, remainingTime);
@@ -68,11 +82,52 @@ public class Game{
         mainPane.setRight(rightVBox);
         BorderPane.setAlignment(rightVBox, Pos.CENTER);
 
+        //Score Animation Timer
+        new AnimationTimer(){
+            public void handle(long currentNanoTime){
+                //Background working refeshing the score
+                if(hasLoched) {
+                    //Setting the new Score
+                    konfettiList = spieler.getKonfetti();
+                    scoreValue = konfettiList.size();
+                    score.setText("Score: "+scoreValue);
+                    hasLoched = false;
+                }
+
+            }
+        }.start();
+
+
+        //Countdown Timer
+        new AnimationTimer(){
+            public void handle(long currentNanoTime){
+                //
+                long elapsed = (System.currentTimeMillis() - lastNanoTimeTimer);
+                elapsed = ((elapsed / 1000));
+                if(elapsed >= 1){
+
+                    //Check for end of Time
+                    if(remainingTimeAvailable == 0){
+                        //todo: End Game and Display Score Screen
+                    }
+
+                    //Changing the Time
+                    remainingTimeAvailable = remainingTimeAvailable - 1;
+                    remainingTime.setText("Zeit: 00:" + remainingTimeAvailable);
+
+                    System.out.println("Fuck off " + elapsed);
+                    lastNanoTimeTimer = System.currentTimeMillis();
+                }
+                else{
+                    //todo: Fehlermeldung an den User
+                }
+                
+                
+            }
+        }.start();
+        
         //Set Window Titel
         stage.setTitle(windowTitle);
         return gameScene;
-
-        //Triggering selfe every 10 milliseconds with an second thread
-        
     }
 }
