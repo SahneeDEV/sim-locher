d93161d6899650215c9b5ce872fe021c4538f2c5
DaNeubi <Lukas.Neubauer97@hotmail.de>
2018-10-22 22:53:12 +0200

    hilfe

--- src/main/java/de/wolc/gui/Game.java
+++ src/main/java/de/wolc/gui/Game.java
@@ -262,7 +262,7 @@ public class Game{
                 abgelegt = false;
             }
             if (abgelegt) {
-                new LocherPapier(Game.this);
+                new LocherPapier(Game.this, spieler);
                 objekt.zerstoeren();
             }
         }
