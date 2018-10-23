216ac252fe03b1c4cef002a1dc8910a2e279fb61
PatrickSachs <kontakt@patrick-sachs.de>
2018-10-23 19:58:24 +0200

    Farbiges Papier

--- src/main/java/de/wolc/gui/PapierObjekt.java
+++ src/main/java/de/wolc/gui/PapierObjekt.java
@@ -8,6 +8,13 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.effect.Blend;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.ColorInput;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.paint.Color;
+import javafx.scene.effect.Light;
+import javafx.scene.effect.Lighting;
 
 public class PapierObjekt {
     private static final Image IMAGE = new Image("de/wolc/gui/images/paper_master.png");
@@ -28,12 +35,22 @@ public class PapierObjekt {
     public PapierObjekt(Game game, Papier papier) {
         this.papier = papier;
         this.game = game;
+        Color farbe = papier.getFarbe().getGuiFarbe();
+
+        Lighting lighting = new Lighting();
+        lighting.setDiffuseConstant(1.0);
+        lighting.setSpecularConstant(0.0);
+        lighting.setSpecularExponent(0.0);
+        lighting.setSurfaceScale(0.0);
+        lighting.setLight(new Light.Distant(45, 45, farbe));
+
         this.drawnPapier = new Rectangle();
         this.drawnPapier.setFill(new ImagePattern(IMAGE));
         this.drawnPapier.setWidth(IMAGE.getWidth());
         this.drawnPapier.setHeight(IMAGE.getHeight());
         this.drawnPapier.setTranslateX(this.zufallsBreite());
         this.drawnPapier.setTranslateY(this.zufallsHoehe());
+        this.drawnPapier.setEffect(lighting);
         this.drawnPapier.setOnMouseDragged((MouseEvent e) -> {
             //Change the location if the cursor has moved
             //TODO: Grab area anpassen
